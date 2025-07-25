6183c8dc4daa2b52bfd26bf6f5f93e362dac3953
Yanze Wu <158328998+Yanze66@users.noreply.github.com>
2025-07-25 11:09:23 -0400

    Update intt_stage7_opt.cc

--- arbitrary-additive-NTT/aie_component/src/intt_stage7_opt.cc
+++ arbitrary-additive-NTT/aie_component/src/intt_stage7_opt.cc
@@ -10,7 +10,7 @@
 
 
 //from now on we use for loop for generalizaton
-void intt_stage7_opt(       input_stream_uint32 * cb_input,output_stream_uint32 * cb_output)
+void intt_stage7_opt(       input_stream_int32 * cb_input,output_stream_int32 * cb_output)
 {
     uint32_t a[64],b[64];
     
